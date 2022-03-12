19a3c57929b922a3f2d7b82d581e8e367e5c93f6
Nicolas RIOT <nicolas.r@aitakes.com>
2022-03-12 11:08:26 +0100

    travis yaml

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,19 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/toto/i);
   expect(linkElement).toBeInTheDocument();
 });
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/toto/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/toto/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+
